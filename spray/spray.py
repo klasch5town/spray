96203fb4b0c8a93cd5d7cb4ebe7698d80dfb40dd
Scharr Klaus <Klaus.Scharr@infineon.com>
2025-04-28 10:03:28 +0200

    A first shot of generating spreadsheet from yaml

--- spray/spray.py
+++ spray/spray.py
@@ -7,7 +7,8 @@ import os.path
 import sys
 import yaml
 
-from openpyxl import load_workbook
+from openpyxl import load_workbook, Workbook
+from openpyxl.styles import Alignment
 
 class Spray():
     """This class handles all 'spreadsheet' aspects.
@@ -18,6 +19,12 @@ class Spray():
             with open(self.yaml_file, 'r') as yfh:
                 self.yct = yaml.safe_load(yfh)
         else: self.yct = None
+
+    def _generate_header(self, ws, data_set):
+        column = ord("A")
+        for key,value in data_set.items():
+            ws[f"{chr(column)}1"] = key
+            column += 1
 
     def create(self, header):
         """Create a 'table' by creating the very first yaml entry as table header.
@@ -33,6 +40,36 @@ class Spray():
         self.yct = yaml.dump(tbl_header)
         with open(self.yaml_file, "w") as yfh:
             yaml.dump(tbl_header, yfh, sort_keys=False)
+
+    def yaml2xls(self, yaml_file, xls_file):
+        # read yaml content
+        with open(yaml_file, 'r') as fh:
+            content = yaml.safe_load(fh)
+        # Create a workbook
+        wb = Workbook()
+        # Get the active worksheet or create a new sheet
+        ws = wb.active
+        row = 0
+        for set in content:
+            if row == 0:
+                self._generate_header(ws, set)
+                row = 2
+                continue
+            column = 1
+            for key,value in set.items():
+                cell = ws.cell(row=row, column=column)
+                if value is not None:
+                    value = value.rstrip("\n")
+                    if value.startswith("-"):
+                        value = "'"+value
+                    cell.value = value
+                    if "\n" in value:
+                        cell.alignment = Alignment(wrap_text=True)
+                column += 1
+            row += 1
+
+        # Save the workbook to a file
+        wb.save(xls_file)
 
     def xls2yaml(self, xls_file, yaml_file):
         wb = load_workbook(filename = 'empty_book.xlsx')
@@ -66,6 +103,7 @@ def main():
     )
     parser.add_argument(
         'input',
+        default="not relevant",
         help="input to handle with the given task"
     )
     parser.add_argument(
@@ -96,6 +134,8 @@ def main():
         spray.create(args.input)
     elif args.task == "xls2yaml":
         spray.xls2yaml(args.xls_file, args.yaml_file)
+    elif args.task == "yaml2xls":
+        spray.yaml2xls(args.yaml_file, args.xls_file)
 
 
 if __name__ == '__main__':
