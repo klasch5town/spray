6e19402afedaf73ddf2b226126ac732a5e8e5cb6
Scharr Klaus <Klaus.Scharr@infineon.com>
2025-04-30 09:15:10 +0200

    Add test for xls2yaml and yaml2xls tasks

---
+++ test/test_xls_generation.py
@@ -0,0 +1,29 @@
+import sys
+import os
+import pytest
+
+from unittest.mock import patch
+
+# Prepare sys.path
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# 'Local' imports
+from helper_functions import remove_file
+from spray.spray import Spray, main
+
+def test_create_xls_file(capfd):
+    """
+    Test the creation of a xls file
+    """
+    test_yaml_file = "./examples/simple_table.yml"
+    test_xls_file = "./build/simple_table.xlsx"
+    remove_file(test_xls_file)
+    # Patch sys.argv with valid arguments
+    with patch("sys.argv", ["spray.py", "-y", test_yaml_file, "-t", "yaml2xls", "-x",test_xls_file , "not-relevant"]):
+        main()
+
+    # Check that the file was created
+    assert os.path.exists(test_xls_file)
+
+    # remove spreadsheet test-file
+    remove_file(test_xls_file)
